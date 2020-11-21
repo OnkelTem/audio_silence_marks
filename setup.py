ac707f17b63a0a72e55859d6a4869fe87b84960a
OnkelTem <aneganov@gmail.com>
2020-11-21 04:07:40 +0300

    fix(dev): minor fixes to setup.py; added LINCESE; fix .gitignore

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="audio_silence_marks",
-    version="0.1",
+    version="0.0.1",
     description="Tool for creating silence marks for audio files",
-    url="https://github.com/onkeltem/audio_silence_marks/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OnkelTem/audio_silence_marks",
     author="OnkelTem",
     author_email="aneganov@gmail.com",
     license="MIT",
-    packages=["audio_silence_marks"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         "docopt",
     ],
     entry_points={
         "console_scripts": ["audio_silence_marks=audio_silence_marks.main:main"],
     },
-    zip_safe=False,
+    python_requires=">=3.6",
 )
