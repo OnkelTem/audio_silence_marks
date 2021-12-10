bde42ff57362a057891aef0f50771519ac79d434
OnkelTem <aneganov@gmail.com>
2021-12-10 10:48:18 +0300

    Minor fix to count marks properly

--- scripts/count-marks.sh
+++ scripts/count-marks.sh
@@ -4,4 +4,4 @@ file="$1"
 
 [[ -f "$file" ]] || { echo "Cannot read input file: '$file'"; exit 1; }
 
-jq -s '.[0].files[0].marklist | length' "$file"
+jq '[.files[].marklist[].id] | length'  "$file"
